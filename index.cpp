e76c67bde8c6e327a72f5ba531f3188364fd24fd
Rahmatul-Karim <rahmatulkarim16@gmail.com>
2026-03-12 23:25:25 +0600

    Create index.cpp

---
+++ index.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+int main() {
+    int num1, num2, sum;
+
+    std::cout << "Enter the first number: ";
+    std::cin >> num1; // Reads input and stores it in num1
+
+    std::cout << "Enter the second number: ";
+    std::cin >> num2; // Reads input and stores it in num2
+
+    sum = num1 + num2;
+
+    std::cout << "The sum of the numbers is: " << sum << std::endl;
+
+    return 0;
+}
